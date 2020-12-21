832f816786b88b51aabb61c35f254b066124641e
hayata1029 <hayatasoccer1029@gmail.com>
2020-12-21 17:50:54 +0900

    Created the Area model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  def index
+    @q = Shop.ransack(params[:q])
+    @shops = @q.result(distinct: true)
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    
   end
 end
