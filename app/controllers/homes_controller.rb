eb75a6d127ee68581834f59e92152099f09284c7
hayata1029 <hayatasoccer1029@gmail.com>
2020-12-21 12:50:40 +0900

    Added nabvar and functions around it.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,6 @@
 class HomesController < ApplicationController
+  def index
+    # @search = Store.ransack(params[:q])
+    # @search_shops = @search.result(distinct: true).order(created_at: "DESC").includes(:shops)
+  end
 end
