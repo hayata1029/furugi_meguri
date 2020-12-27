44ae0c62653a932686c5a0df6e7f687f673ab1d3
hayata1029 <hayatasoccer1029@gmail.com>
2020-12-27 10:16:55 +0900

    Fix Stores controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -6,17 +6,7 @@ class StoresController < ApplicationController
   end
 
   def search
-    @search = Store.search(search_params)
+    @search = Store.ransack(params[:q])
     @stores = @search.result(distinct: true).page(params[:page]).per(5)
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
-  end
-
-  private
-  def search_params
-    params.require(:q).permit!
   end
 end
