89469ae70ab2b4ba728f76e856d684c24c474ab7
hayata1029 <hayatasoccer1029@gmail.com>
2020-12-30 12:04:06 +0900

    Add resoucers for stores

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'stores#index'
   get 'index',  to: 'areas#index'
   get 'search', to: 'stores#search'
+  resources :stores, only: [:search, :show]
   devise_for :users
 end
