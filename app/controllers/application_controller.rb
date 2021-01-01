e0a4244d26f8d18585fffd68ea157c807241ff04
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-01 10:11:54 +0900

    Create a guest login function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,32 @@
 class ApplicationController < ActionController::Base
+  before_action :store_location
+
   def index
     @q = Shop.ransack(params[:q])
     @shops = @q.result(distinct: true)
   end
+
+  private
+
+  def sign_in_required
+    redirect_to new_user_session_url unless user_signed_in?
+  end
+
+  # ここでURLを覚えておく
+  def store_location
+    return unless request.get? 
+    if (request.path != "/stores/index"  &&
+        request.path != "/stores/search" &&
+        request.path != "/stores/show"   &&
+        request.path != "/users/sign_in" &&
+        !request.xhr?)
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  # ここで覚えておいたパスを返す
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end  
+
 end
