bce3d004c50026537f6d217339b86fd6f27c48eb
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-05 08:18:12 +0900

    Add create controller in woms

--- app/controllers/woms_controller.rb
+++ app/controllers/woms_controller.rb
@@ -1,8 +1,23 @@
 class WomsController < ApplicationController
+  before_action :set_store,   only: [:index, :new, :create]
+  before_action :set_area,    only: [:index, :new, :create]
+  before_action :correct_wom, only: [:destroy]
+  def index
+    @wom = Wom.find_by(params[:id])
+  end
+
   def new
+    @wom = Wom.new
   end
 
   def create
+    @wom = current_user.woms.build(wom_params)
+    if @wom.save
+      render 'woms/index'
+    else
+      render 'woms/new'
+    end
+    @wom.user_id = current_user.id
   end
 
   def edit
