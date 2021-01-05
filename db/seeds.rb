f49c3b520e162f3fd39edd67341b93d0c92ae3b8
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-05 19:12:18 +0900

    Delete seeding data about wom

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,26 +41,3 @@ store_images = StoreImage.create([
 # メインのサンプルユーザーを１人作成する
 User.create!(email: "example@railstutorial.com",
              password:              "foobar")
-
-Wom.create!([
-  {user_id: 1, store_id: 1, content: "すごい", title: "とてもいいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 2, content: "すごいいい", title: "かなりとてもいいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 3, content: "かっこいい", title: "かっこよくていいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 4, content: "安い", title: "服が安くていいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 5, content: "質がいい", title: "とても質のいいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 6, content: "すごいひろい", title: "広いお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 7, content: "再来店", title: "もう一度きたいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 8, content: "きれい", title: "きれいなお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 9, content: "びっくり", title: "みたことないお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 10, content: "かなりいい", title: "初めて来店しました", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 11, content: "かわいい", title: "可愛いお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 12, content: "満足", title: "何度でもいきたいです", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 13, content: "めっちゃいい", title: "めっちゃいいお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 14, content: "やばい", title: "１０着かってしまいました", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 15, content: "かなりすごい", title: "また来たいです", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 16, content: "やっぱりいい", title: "久々でしたがよかったです", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 17, content: "高音", title: "とても高く売却してもらえました", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 18, content:  "無限にきたい", title: "何回でもいきたいです", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 19, content: "すごく好み", title: "好きなタイプのお店です", visit_date: "2021-01-29"},
-  {user_id: 1, store_id: 20, content: "またいきたい", title: "今月３回目です笑", visit_date: "2021-01-29"}
-])
