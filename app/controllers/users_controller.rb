795651a7c9a3c6a307520105b309800e82d0c070
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-10 15:03:54 +0900

    Create user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def edit
-    @user = User.find(current_user.id)
-  end
-
-  def update
-  end
 end
