891c7b26c9f68d223a9fd954d9d0a8ffbdb447bd
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-11 16:05:23 +0900

    Create item post function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :set_store,    only: [:show, :edit, :new, :create, :update]
+  before_action :set_area,     only: [:show, :edit, :new, :create, :update]
+  before_action :set_wom,      only: [:edit, :update, :destroy]
+  before_action :set_item,     only: [:edit, :update, :destroy]
+  before_action :set_favorite
+
   def index
+    @items = Item.all
+    @user = User.find(current_user.id)
   end
 
-  def create
+  def show
+    @item = Item.find_by(params[:item_id])
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = current_user.items.build(items_params)
+    if @item.save
+      redirect_to store_items_path(@store.id)
+    else
+      redirect_to new_store_item_path(@user.id)
+    end
+    @item.user_id = current_user.id
   end
 
   def edit
@@ -15,5 +35,48 @@ class ItemsController < ApplicationController
   end
 
   def destory
+    @item.destroy
+    redirect_to store_items_path(@store.id)
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:store_id, :name, :price, :image)
+  end
+
+  def set_item
+    @item = Item.(params[:id])
+  end
+
+  def set_wom
+    @wom = Wom.find(params[:id])
+  end
+
+  def set_store
+    @store = Store.find(params[:store_id])
+  end
+
+  def set_area
+    @area = Area.find(@store.area_id)
+  end
+
+  def correct_wom
+    @wom = Wom.find(params[:id])
+    redirect_to root_url if @wom.nil?
+  end
+
+  def update_params
+    params.require(:wom).permit(:name, :price, :image)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def set_favorite
+    if user_signed_in?
+      @favorite = Favorite.find_by(store_id: params[:store_id], user_id: current_user.id)
+    end
   end
 end
