104168584d1ba78933c80f9bb0d15a7d8a908418
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-11 09:07:07 +0900

    Remove routing that is no longer needed.

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   resources :users do
     resources :favorites, only: [:index]
   end
-  resources :users, :only => [:show, :edit]
+  resources :users, only: [:show]
 end
