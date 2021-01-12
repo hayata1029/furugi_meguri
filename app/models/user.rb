bdcd976917b62072f84df444b155d9a3aa7883c2
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-12 21:16:04 +0900

    Add name for guest user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'テストユーザー', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
