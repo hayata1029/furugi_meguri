6c555c9d99b941bb37e5320d683e57975c0bf0a6
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-12 12:35:01 +0900

    Add Changing the data type of the price column in the item table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_003609) do
+ActiveRecord::Schema.define(version: 2021_01_12_021418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_003609) do
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
     t.string "image"
-    t.string "price"
+    t.integer "price"
     t.bigint "user_id", null: false
     t.bigint "store_id", null: false
     t.datetime "created_at", precision: 6, null: false
