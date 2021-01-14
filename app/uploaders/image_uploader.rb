f9dab0213fec2671679f0de73e7e8a15cf5f7db0
hayata1029 <hayatasoccer1029@gmail.com>
2021-01-14 11:58:56 +0900

    Changed the test data because we renamed the column.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
-
+  storage :file
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
